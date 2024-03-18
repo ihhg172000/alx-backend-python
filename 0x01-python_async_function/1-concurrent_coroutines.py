366eb28e8dbbe82cc2c707579d02672054534339
Islam Hany <ihhg.developer@gmail.com>
2024-03-19 00:43:44 +0200

    fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,7 +3,6 @@
 1-concurrent_coroutines.py
 """
 from typing import List
-import random
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -15,10 +14,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     spawns 'wait_random' 'n' times with the specified 'max_delay',
     and returns the list of all the delays.
     """
-    delays = [random.uniform(0, max_delay) for _ in range(n)]
-    delays.sort()
-
-    for delay in delays:
-        await asyncio.sleep(delay)
-
-    return delays
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
